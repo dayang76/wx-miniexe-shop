fbf845f1deef875c8f3c258f00fb79d4bad24c30
dayang <dayang.cn>
2023-01-18 15:21:56 +0800

    object define test

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // app.js
 App({
   onLaunch() {
+    Object.defineProperties.myprote = function () {
+      console.log('my object prote');
+    }
+    const myprote = {}
+    console.log(myprote.myprote);
   },
 })
 // master分支第二次修改
